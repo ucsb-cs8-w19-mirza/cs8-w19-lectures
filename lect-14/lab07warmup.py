852dcddd4992e5bffc083a34fffebf9aab8602f0
Diba Mirza <dimirza@cs.ucsb.edu>
2019-03-05 15:24:49 -0800

    lab 07

---
+++ lect-14/lab07warmup.py
@@ -0,0 +1,13 @@
+# Find all the valid words
+# Given a string of letters: myLetters
+# Given a file (filename)
+# return a list of all the words that
+# can be made with myLetters and that
+# exist in the file
+
+# Problem 1: Given a file,
+# return a list of all the words
+# in the file
+# result is a list of all the words
+# in dictionary 
+
